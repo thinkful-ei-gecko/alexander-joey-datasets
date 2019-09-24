3d5bd87fec724e835ab7fe3d1ca9628994d72096
Alexander Fukui <alexander.fukui@gmail.com>
2019-09-24 15:44:25 -0500

    Finished server! Running filters

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const movies = require('./movies.js');
+
+let moviesResults = [];
 
 console.log(process.env.API_TOKEN);
 const app = express();
@@ -13,8 +17,14 @@ app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req,res,next){
-  console.log('validateBearerToken middleware');
+  const authToken = req.get('Authorization');
+  const apiToken = process.env.API_TOKEN;
 
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  next();
 });
 
 app.get('/movie', (req, res) => {
@@ -22,23 +32,26 @@ app.get('/movie', (req, res) => {
   const country = req.query.country;
   const avgVote = req.query.avg_vote;
 
- 
+  
+
   if(genre){
-    movies.filter( movie => 
+    moviesResults = movies.filter( movie => 
       movie.genre.toLowerCase().includes(genre.toLowerCase()));
-    
   }
+
   if(country){
-    movies.filter( movie => 
+    moviesResults = movies.filter( movie => 
       movie.country.toLowerCase().includes(country.toLowerCase()));
     
   }
+
   if(avgVote){
-    movies.filter( movie => 
+    moviesResults = movies.filter( movie => 
       movie.avg_vote >= parseInt(avgVote)
     );
-    
   }
+
+  return res.json(moviesResults);
 });
 
 const PORT = 8000;
